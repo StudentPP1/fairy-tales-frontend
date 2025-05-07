302595e758103899cd16ef8c2d3f99e213470cd6
StudentPP1 <mishamakytonin@gmail.com>
2025-05-07 23:18:28 +0300

    fix: update router

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -1,4 +1,5 @@
-import { useAuth } from "@/context/AuthContext";
+import { AuthContext } from "@/context/AuthContext";
+import type { AuthState } from "@/context/AuthContext";
 import AuthPage from "@/pages/Auth";
 import CreateStoryPage from "@/pages/CreateStory";
 import HomePage from "@/pages/Home";
@@ -6,18 +7,17 @@ import LikedPage from "@/pages/Liked";
 import SearchPage from "@/pages/Search";
 import UserSettingsPage from "@/pages/Settings";
 import StoryDetailsPage from "@/pages/StoryDetails";
-import React, { type JSX } from "react";
+import React, { useContext, type JSX } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 interface ProtectedRouteProps {
   children: JSX.Element;
 }
 
-// Компонент для захисту приватних маршрутів
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { accessToken } = useAuth();
-  if (!accessToken) {
-    return <Navigate to="/" replace />;
+  const { user } = useContext<AuthState>(AuthContext);
+  if (!user) {
+    return <Navigate to="/login" replace />;
   }
   return children;
 };
@@ -27,24 +27,29 @@ const AppRoutes: React.FC = () => {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<AuthPage />} />
-      <Route path="/favorites" element={<LikedPage />} />
       <Route path="/search" element={<SearchPage />} />
-      <Route path="/setting" element={<UserSettingsPage />} />
-      <Route path="/story/:id" element={<StoryDetailsPage />} />
-      <Route path="/create-story" element={<CreateStoryPage />} />
-      {/* Приклад захищеного маршруту */}
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <div className="p-8">
-              <h1 className="text-3xl font-bold">Dashboard</h1>
-              <p>This is a protected dashboard page.</p>
-            </div>
-          </ProtectedRoute>
-        }
-      />
-      {/* Редірект або обробка 404 */}
+
+      <Route path="/favorites" element={
+        <ProtectedRoute>
+          <LikedPage />
+        </ProtectedRoute>
+      }/>
+      <Route path="/setting" element={
+        <ProtectedRoute>
+          <UserSettingsPage />
+        </ProtectedRoute>
+      }/>
+      <Route path="/story/:id" element={
+        <ProtectedRoute>
+          <StoryDetailsPage />
+        </ProtectedRoute>
+      }/>
+      <Route path="/create-story" element={
+        <ProtectedRoute>
+          <CreateStoryPage />
+        </ProtectedRoute>
+      }/>
+      
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
